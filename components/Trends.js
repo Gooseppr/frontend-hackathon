4c61703545c2ea5c2061a1be771a285745025411
Gooseppr <gregoire.elmacin@gmail.com>
2025-11-21 17:05:10 +0100

    modif ip

--- components/Trends.js
+++ components/Trends.js
@@ -10,7 +10,7 @@ function Trends() {
   const [trendsData, setTrendsData] = useState([]);
 
   useEffect(() => {
-    fetch(`http://localhost:3000/tweets/trends/${user.token}`)
+    fetch(`http://34.155.75.122:3000/tweets/trends/${user.token}`)
       .then(response => response.json())
       .then(data => {
         data.result && setTrendsData(data.trends);
